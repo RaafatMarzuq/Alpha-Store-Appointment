fc07c5db7e60a34561da899190692751059343f6
RaafatMarzuq <57416873+RaafatMarzuq@users.noreply.github.com>
2024-02-18 21:26:13 +0200

    new rout

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,20 @@
 import './Home.css';
 
 import Button from '../../components/Button/Button';
+import {useNavigate} from "react-router-dom"
+
 
 const Home = ({logo})=> {
-   const onUserClick = ()=>{
-     window.location.href=process.env.PUBLIC_URL +'/users';
-   }
-   const onAdminClick = ()=>{
-     window.location.href= process.env.PUBLIC_URL +'/pass';
-   }
+  const [toPage,setToPage] = useState("")
+  let navigate = useNavigate();
+    useEffect(()=> {
+     toPage ==="Home"? navigate(`/`):navigate(`/${toPage}`)
+    },[toPage,navigate])
+   
+    function onClick(page){
+      setToPage(page)
+      
+    }
 
   return (
    
@@ -17,8 +23,8 @@ const Home = ({logo})=> {
         <div className="home">     
           <div className='input-container' >
             <img src={logo} className="App-logo" alt="logo" />
-            <Button  id='users'  buttonName='לקוח'  onClick={onUserClick}/>
-            <Button  id='admin'  buttonName= 'אדמין' onClick={onAdminClick} /> 
+            <Button  id='users'  buttonName='לקוח'  onClick={()=>{onClick("users")}}/>
+            <Button  id='admin'  buttonName= 'אדמין' onClick={()=>{onClick("pass")}} /> 
             </div>
         </div>
       </body>
